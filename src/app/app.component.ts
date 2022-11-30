da5aa9f2648cfcae737418c54f2fba3e9a6d9ff8
Filip Smyczek <smyczekf@interia.pl>
2022-11-30 14:08:57 +0100

    Changed into catch game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  counter = 0;
-
-  constructor(){
+  words: string[] = ['division','frank','user','heal','crouch','excitement',
+                  'solution', 'blast', 'outlook', 'short', 'production', 'iron',
+                  'earwax', 'hunter', 'conscious']
+  
+  chosenWord: string = '';  
+  toGuess: string = '';
+  score = 0;
+  isVictoryVisible = false;
+  isFailureVisible = false;
+  isPanelVisible = false;
+constructor(){
     window.setInterval(() => {
-      this.counter++;
-    }, 1000);
+      this.chosenWord = this.words[Math.floor(Math.random() * this.words.length)];
+    }, 500);
+    this.generateToGuess();
   } 
+
+  checkResult(){
+    if(this.chosenWord === this.toGuess){
+      this.score++;
+      this.generateToGuess();
+    }
+    else this.score--;
+    
+    if(this.score > 5){
+      this.score = 0;
+      this.isVictoryVisible = true;
+      this.isPanelVisible = true;
+      setTimeout(() => {
+        this.isPanelVisible = false;
+        this.isVictoryVisible = false;
+      }, 3000)
+    }
+    else if(this.score < 0){
+      this.score = 0;
+      this.isFailureVisible = true;
+      this.isPanelVisible = true;
+      setTimeout(() => {
+        this.isFailureVisible = false;
+        this.isPanelVisible = false;
+      }, 3000)
+    }
+  }
+
+  generateToGuess(){
+    this.toGuess = this.words[Math.floor(Math.random() * this.words.length)];
+  }
+  // reset(){
+  //   this.counter = 0;
+  // }
+
+  // update(e: Event){
+  //   this.counter = parseInt((e.target as HTMLInputElement).value);
+  // }
+
+  
 }
